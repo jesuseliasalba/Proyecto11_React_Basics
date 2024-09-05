479b85abac9ad354fc9d7ea22b7cff3167478c59
Jesus Elias Alba <jesuseliasalba2000@gmail.com>
2024-09-06 00:38:27 +0200

    Start main weather zone

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,24 +7,24 @@ const Header = () => {
 
   const [city, setCity] = useState("Desconocido")
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      fetch(`https://us1.locationiq.com/v1/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json&key=pk.e65df5358c57261e3f376d4caf32a11b`,{method: 'GET', headers: {accept: 'application/json'}})
-      .then(res => res.json())
-      .then(res => setCity(`${res.address.city}, ${res.address.country}`))
-      .catch(err => console.error('error:' + err));      
-      },() => {
-        fetch('https://api.ipdata.co/?api-key=298683c76a6c6bee1a5970f2e90d478413ba4c45b55d28c87093b610', {headers: {'Accept':'application/json'}})
-        .then(res => res.json())
-        .then(res => setCity(`${res.city}, ${res.country_name}`))
-        .catch(err => console.error(err))
-      } )
-  }, [])
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     fetch(`https://us1.locationiq.com/v1/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json&key=pk.e65df5358c57261e3f376d4caf32a11b`,{method: 'GET', headers: {accept: 'application/json'}})
+  //     .then(res => res.json())
+  //     .then(res => setCity(`${res.address.city}, ${res.address.country}`))
+  //     .catch(err => console.error('error:' + err));      
+  //     },() => {
+  //       fetch('https://api.ipdata.co/?api-key=298683c76a6c6bee1a5970f2e90d478413ba4c45b55d28c87093b610', {headers: {'Accept':'application/json'}})
+  //       .then(res => res.json())
+  //       .then(res => setCity(`${res.city}, ${res.country_name}`))
+  //       .catch(err => console.error(err))
+  //     } )
+  // }, [])
 
   return (
     
-    <header className='displayFlex-row'>
-      <div className='displayFlex-row divCity'>
+    <header className='displayFlex row container'>
+      <div className='displayFlex row divCity'>
         <i className="fa-solid fa-location-dot"></i>
         <p>{city ? city : "Desconocida"}</p>
       </div>
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,7 +4,18 @@ import "./home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='container displayFlex row topContent'>
+      <div className='timeTown displayFlex column'>
+        <div className='optionsTime displayFlex row'>
+          <h2>The time on your town</h2>
+          <h2>hola</h2>
+        </div>
+        <div className='timeDays'></div>
+      </div>
+      <div className='rainChance'>
+        <h2>rain for example</h2>
+      </div>
+    </section>
   )
 }
 
